713b5865444eef59cc8a585a1e4829e0aec6e81b
alex <al3xback@gmail.com>
2024-06-09 19:30:48 +0700

    added extention

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header/Header';
-import ResultGrid from './components/ResultsGrid/ResultGrid';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header.jsx';
+import ResultGrid from './components/ResultsGrid/ResultGrid.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
 	return (
